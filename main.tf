c5aa611b30b89e99bd6f64d26ef870df7cd0222f
Rochak Agrawal <36627691+rochakagrawal89@users.noreply.github.com>
2022-07-19 15:25:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,13 @@ provider "aws" {
   secret_key = "2a+cn+ky5Cs/ZmYzirRExh+TIUYQpnAYCDbm9mLR"
 }
 
-resource "aws_instance" "myec2" {
-   ami = "ami-082b5a644766e0e6f"
-   instance_type = "t2.micro"
+variable "elb_names" {
+  type = list
+  default = ["dev-loadbalancer", "stage-loadbalanacer","prod-loadbalancer"]
+}
+
+resource "aws_iam_user" "lb" {
+  name = var.elb_names[count.index]
+  count = 3
+  path = "/system/"
 }
